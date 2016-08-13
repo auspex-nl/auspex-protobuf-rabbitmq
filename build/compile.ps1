8a813db158b35240a84220a0d78572c622fa857f
auspex-nl <github@auspex.nl>
2016-08-13 15:46:27 +0200

    updated buildscript to do dotnet restore

--- build/compile.ps1
+++ build/compile.ps1
@@ -1,4 +1,5 @@
 ls ..\src\proto\*.proto | %{.\protoc.exe --proto_path=..\src\proto\ --csharp_out=..\src\csharp\auspex-protobuf-rabbitmq-entities\Auspex.Protobuf.RabbitMQ.Entities\Messages\ ("..\src\proto\" + $_.Name)}
 cd ..\src\csharp\auspex-protobuf-rabbitmq-entities\Auspex.Protobuf.RabbitMQ.Entities\
+dotnet restore
 dotnet build
 dotnet pack
