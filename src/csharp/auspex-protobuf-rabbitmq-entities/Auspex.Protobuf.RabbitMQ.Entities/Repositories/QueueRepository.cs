2d17c07919a14337c9dd7ddb50e4ac6f6d7690ba
auspex-nl <github@auspex.nl>
2016-08-13 16:22:55 +0200

    updated repository

--- src/csharp/auspex-protobuf-rabbitmq-entities/Auspex.Protobuf.RabbitMQ.Entities/Repositories/QueueRepository.cs
+++ src/csharp/auspex-protobuf-rabbitmq-entities/Auspex.Protobuf.RabbitMQ.Entities/Repositories/QueueRepository.cs
@@ -8,9 +8,49 @@ namespace Auspex.Protobuf.RabbitMQ.Entities.Repositories
     /// <summary>
     /// General rabbitmq queue repository for generic type where T is a Google Protobuf message
     /// </summary>
-    /// <typeparam name="T"></typeparam>
-    public class QueueRepository<T> where T: Google.Protobuf.IMessage
+    /// <typeparam name="MessageType"></typeparam>
+    public class QueueRepository<MessageType> : IDisposable where MessageType: class, Google.Protobuf.IMessage
     {
+        private readonly EasyNetQ.IBus messageQueueBus;
+
+        public QueueRepository(string hostName)
+        {
+            messageQueueBus = EasyNetQ.RabbitHutch.CreateBus($"host={hostName}");            
+        }
+
+        public void Subscribe(Action<MessageType> action)
+        {
+            messageQueueBus.Subscribe<MessageType>(typeof(MessageType).Name,
+                action
+                );
+        }
+
+        public void Publish(MessageType message)
+        {
+            messageQueueBus.Publish(message);
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    this.messageQueueBus.Dispose();
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+        #endregion
 
     }
 }
