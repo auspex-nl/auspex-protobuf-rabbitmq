36739177c25c27aa192b3d1a3011b6079cb6789b
auspex-nl <github@auspex.nl>
2016-08-13 16:00:23 +0200

    updated build script, added gitignore for messages source files

--- build/compile.ps1
+++ build/compile.ps1
@@ -1,5 +1,13 @@
+param([string]$buildConfig) 
+
+if (-Not $buildConfig)
+{
+    Write-Host "Build Configuration parameter not set. Quitting.." 
+    exit
+}
+
 ls ..\src\proto\*.proto | %{.\protoc.exe --proto_path=..\src\proto\ --csharp_out=..\src\csharp\auspex-protobuf-rabbitmq-entities\Auspex.Protobuf.RabbitMQ.Entities\Messages\ ("..\src\proto\" + $_.Name)}
 cd ..\src\csharp\auspex-protobuf-rabbitmq-entities\Auspex.Protobuf.RabbitMQ.Entities\
 dotnet restore
-dotnet build
-dotnet pack
+dotnet build --configuration $buildConfig
+dotnet pack --configuration $buildConfig
